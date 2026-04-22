9a4d6434dad38cb2648ca1e47d51de8ec3fe9072
priyachintapalli <priyachintapalli95@gmail.com>
2026-04-22 10:35:11 +0530

    Create even_odd.c

---
+++ even_odd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter the num:");
+    scanf("%d",&n);
+    if(n%2==0){
+        printf("even no");
+    }else{
+        printf("odd no");
+    }
+    return 0;
+}
+    
